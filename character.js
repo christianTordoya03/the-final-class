29e322ab5c8039bdb8216103c56b1c1d0cddd24d
ChristianTordoya <cristian14021997@gmail.com>
2023-06-24 13:33:49 -0500

    first commit

---
+++ character.js
@@ -0,0 +1,31 @@
+new Vue ({
+    el: '#app',
+
+    data (){
+        return {
+            info: null,
+            meta: null
+        }
+    },
+
+    mounted (){
+        axios
+            .get('https://rickandmortyapi.com/api/character')
+            .then(response => (
+                this.info = response.data.results,
+                this.meta = response.data.info
+            ))
+    },  
+
+    methods: {
+        siguiente() {
+            axios
+            .get(this.meta.next)
+            .then(response => (
+                this.info = response.data.results,
+                this.meta = response.data.info
+            ))
+
+        }
+    }
+})
